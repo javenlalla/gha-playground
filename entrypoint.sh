3e5a2c308901c48cc3310ad0583534a1734fbb8f
Javen Lalla <javen.lalla@gmail.com>
2024-03-31 18:49:45 +0200

    refactor: simplify conditional to boolean flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+dev_env=false
+if [[ $APP_ENV != "prod" && $APP_ENV != "test" ]]; then
+    dev_env=true
+fi
+
 # Configure the .env file.
 > .env
 echo "MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0" >> .env
 
-# # Configure the .env file.
+# Configure the .env file.
 if [[ ! -z $DB_HOST ]]; then
     export DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?serverVersion=mariadb-10.8.6&charset=utf8mb4"
     echo "Database URL configured as: ${DATABASE_URL}"
@@ -27,7 +32,7 @@ else
     echo "DATABASE_URL=${DATABASE_URL}" >> .env
 fi
 
-if [[ $APP_ENV != "prod" && $APP_ENV != "test" ]]; then
+if [[ "$dev_env" = true ]]; then
     # Note: it is intentional that the APP_ENV is written to the .env file instead of declared in the Dockerfile.dev because the container Environment Variables take precedence over the .env file(s).
     # As a result, running tests in the development environment fail due to not switching to the test environment programmatically when running. The following error is thrown:
     # Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException: You have requested a non-existent service "test.service_container". Did you mean this: "service_container"?
@@ -41,7 +46,7 @@ fi
 export APP_SECRET=$(openssl rand -base64 40 | tr -d /=+ | cut -c -32)
 echo "APP_SECRET=${APP_SECRET}" >> .env
 
-if [[ $APP_ENV != "prod" && $APP_ENV != "test" ]]; then
+if [[ "$dev_env" = true ]]; then
   echo "Installing composer dependencies. This will take a few minutes since xdebug is installed."
   composer install
 fi
@@ -72,7 +77,7 @@ if [[ ! -z $DB_HOST || ! -z $DB_SQLITE_FILENAME || ! -z $DATABASE_URL ]]; then
 fi
 
 # Fix permissions, espeically to address `var` folder not being writeable for cache and logging.
-if [[ $APP_ENV != "prod" && $APP_ENV != "test" ]]; then
+if [[ "$dev_env" = true ]]; then
     if [ -d "/var/www/app" ]; then
         # Fix permissions, especially to address `var` folder not being writeable for cache and logging.
         echo "Fixing application folder permissions."
